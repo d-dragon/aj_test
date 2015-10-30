a5a334654622a5f6af27a0c3a9af4fd5c01ccd56
tuanngo <tuanngo@tuanngo.verik.iot>
2015-10-30 11:27:30 +0700

    Add a new OnboardingTest constructor is able to get input from std::string[] and convert into correct value for later using.

--- src/onboarding/OnboardingTest.cc
+++ src/onboarding/OnboardingTest.cc
@@ -16,13 +16,20 @@
 #include <qcc/Log.h>
 #include <AJInitializer.h>
 #include "OnboardingTest.h"
-
-using namespace ajn;
 //using namespace services;
 /*
     Constructor and Destructors
 */
-
+OnboardingTest::OnboardingTest(std::string input[]){
+    ajInitial = new AJInitializer();
+    if (ajInitial->Initialize() != ER_OK) {
+        std::cout << "Unable to Initialize AllJoyn " << std::endl;
+    }
+    *interfaces         = {"org.alljoyn.Onboarding"};
+    mbusAttachment      = NULL;
+    mcbFlags            = 0;
+    GetInfoFromString(input);
+}
 OnboardingTest::OnboardingTest(long flags){
 // Initialize AllJoyn
     ajInitial = new AJInitializer();
@@ -95,5 +102,69 @@ QStatus OnboardingTest::FinishBusAttachment(){
     delete mbusAttachment;
     delete srpKeyXListener;
     delete announceHandler;
+    return ER_OK;
+}
+
+void OnboardingTest::GetInfoFromString(std::string input[]){
+    int i;
+    if (!input[0].empty()) mWifiConnectionInfo.SSID.assign(input[0].c_str());
+    if (!input[1].empty()) mWifiConnectionInfo.passcode.assign(input[1].c_str());
+    for ( i = 0; i< sizeof (smWifiAuthType)/sizeof(struct WifiAuthenticationType); i++)
+    {
+        if (input[2].compare(smWifiAuthType[i].Name) == 0){
+            mWifiConnectionInfo.authType = smWifiAuthType[i].Val;
+            break;
+        }
+    }
+    if (input[3].compare("on") == 0) mcbFlags |= GET_VERSION;
+    if (input[4].compare("on") == 0) mcbFlags |= GET_STATE;
+    if (input[5].compare("on") == 0) mcbFlags |= GET_LAST_ERR;
+    if (input[6].compare("on") == 0) mcbFlags |= GET_SCAN_INFO;
+    if (input[7].compare("on") == 0) mcbFlags |= CONF_AND_CONNECT_WIFI;
+    if (input[8].compare("on") == 0) mcbFlags |= OFF_BOARD_FROM;
+}   
+
+// Create bus attachment
+
+QStatus OnboardingTest::CreateBusAttachment(){
+    QStatus status;
+    mbusAttachment = new BusAttachment("OnboardingClient", true);
+
+    status = mbusAttachment->Start();
+    if (ER_OK == status) {
+        std::cout << "BusAttachment started." << std::endl;
+    } else {
+        std::cout << "Unable to start BusAttachment. Status: " << QCC_StatusText(status) << std::endl;
+        return ER_FAIL;
+    }
+
+    status = mbusAttachment->Connect();
+    if (ER_OK == status) {
+        std::cout << "Daemon Connect succeeded." << std::endl;
+    } else {
+        std::cout << "Failed to connect daemon. Status: " << QCC_StatusText(status) << std::endl;
+        return ER_FAIL;
+
+    }
+
+    srpKeyXListener = new SrpKeyXListener();
+    status = mbusAttachment->EnablePeerSecurity("ALLJOYN_SRP_KEYX ALLJOYN_ECDHE_PSK", srpKeyXListener, "/.alljoyn_keystore/central.ks", true);
+
+    if (ER_OK == status) {
+        std::cout << "EnablePeerSecurity called." << std::endl;
+    } else {
+        std::cout << "ERROR - EnablePeerSecurity call FAILED with status " << QCC_StatusText(status) << std::endl;
+        return ER_FAIL;
+    }
+
+    announceHandler = new AboutListenerHandlerImpl(mbusAttachment, mcbFlags, mWifiConnectionInfo);
+    mbusAttachment->RegisterAboutListener(*announceHandler);
+
+    status = mbusAttachment->WhoImplements(interfaces, sizeof(interfaces) / sizeof(interfaces[0]));
+    if (ER_OK == status) {
+        std::cout << "WhoImplements called." << std::endl;
+    } else {
+        std::cout << "ERROR - WhoImplements failed." << std::endl;
+    }
     return ER_OK;
 }
--- src/onboarding/OnboardingTest.h
+++ src/onboarding/OnboardingTest.h
@@ -5,22 +5,39 @@
 #include <AboutListenerHandlerImpl.h>
 #include <SrpKeyXListener.h>
 #include <AJInitializer.h>
+#include "common_def.h"
 #define DBGE 1
+using namespace ajn;
+using namespace services;
+
 class OnboardingTest{
 
 public:
 	/*
-	Intialize and Destroy AJ initialization
+		Intialize and Destroy AJ initialization
+		cbFlgs: list cb function will be called
 	*/
-	OnboardingTest(long);
+	OnboardingTest(long cbFlgs);
+	/*
+		Get wifi info, cb function via input[]
+	*/
+	OnboardingTest(std::string input[]);
 	~OnboardingTest();
 	/*
-		Create Bus Attachment
+		This function need initialize from constructor OnboardingTest(long);
 	*/
 	QStatus CreateBusAttachment(ajn::services::OBInfo oBInfo);
+
+	/*
+		This function need initialize from constructor OnboardingTest(std::string input[]);
+	*/
+	QStatus CreateBusAttachment();
+
 	QStatus FinishBusAttachment();
 
 private:
+	// Convert input into suitable value of wifi info, cb functions
+	void GetInfoFromString(std::string[]);
 	ajn::BusAttachment* mbusAttachment;
 	AboutListenerHandlerImpl* announceHandler;
 	SrpKeyXListener* srpKeyXListener;
@@ -28,7 +45,19 @@ private:
 
 	AJInitializer *ajInitial;
 	long mcbFlags;
-
+	ajn::services::OBInfo mWifiConnectionInfo;
+	struct WifiAuthenticationType smWifiAuthType[10]={
+	    {"WPA2_AUTO",   WPA2_AUTO},                     //!< WPA2_AUTO authentication
+	    {"WPA_AUTO",    WPA_AUTO},                      //!< WPA_AUTO authentication
+	    {"ANY",         ANY},                           //!< ANY authentication
+	    {"OPEN",        OPEN},                          //!< OPEN authentication
+	    {"WEP",         WEP},                           //!< WEP authentication
+	    {"WPA_TKIP",    WPA_TKIP},                      //!< WPA_TKIP authentication
+	    {"WPA_CCMP",    WPA_CCMP},                      //!< WPA_CCMP authentication
+	    {"WPA2_TKIP",   WPA2_TKIP},                     //!<WPA2_TKIP authentication
+	    {"WPA2_CCMP",   WPA2_CCMP},                     //!<WPA2_CCMP authentication
+	    {"WPS",         WPS}                            //!<WPS authentication
+	};
 };
 
 #endif /* ONBOARDINGTEST_H_ */
